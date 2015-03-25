dd77b0a0731b2d3ffc3f7d0c85c2cf1364a10a15
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-03-25 15:29:38 +0100

    adding soft/env description for local GCC and LLVM

---
+++ env/compiler-gcc-local-linux-32/env.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+# CK generated script
+
+# Soft UOA         = compiler.gcc (3a8a82fa40bf992d) 
+# Host OS UOA      = linux-64 (4258b5fe54828a50)
+# Target OS UOA    = linux-32 (fb525e34243f49c1)
+# Target OS bits   = 32
+# Tool version     = local
+# Tool int version = 88888
+
+export CK_AR=ar
+export CK_ASM_EXT=.s
+export CK_CC=gcc
+export CK_COMPILER_FLAGS_OBLIGATORY=
+export CK_CXX=g++
+export CK_DLL_EXT=.so
+export CK_EXE_EXT=.out
+export CK_EXTRA_LIB_DL=-ldl
+export CK_EXTRA_LIB_M=-lm
+export CK_F90=gfortran
+export CK_F95=gfortran
+export CK_FC=gfortran
+export CK_FLAGS_CREATE_ASM=-S
+export CK_FLAGS_CREATE_OBJ=-c
+export CK_FLAGS_DLL="-shared -fPIC"
+export CK_FLAGS_DLL_EXTRA=
+export CK_FLAGS_OUTPUT="-o "
+export CK_FLAGS_STATIC_BIN="-static -fPIC"
+export CK_FLAGS_STATIC_LIB=-fPIC
+export CK_FLAG_PREFIX_INCLUDE=-I
+export CK_FLAG_PREFIX_VAR=-D
+export CK_LB="ar rcs"
+export CK_LB_OUTPUT="-o "
+export CK_LD_FLAGS_EXTRA=
+export CK_LIB_EXT=.a
+export CK_MAKE=make
+export CK_OBJDUMP="objdump -d"
+export CK_OBJ_EXT=.o
+export CK_OPT_SIZE=-Os
+export CK_OPT_SPEED=-O3
+export CK_OPT_SPEED_SAFE=-O2
+export CK_PLUGIN_FLAG=-fplugin=
+export CK_PROFILER=gprof
+
---
+++ env/compiler-llvm-local-linux-64/env.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+# CK generated script
+
+# Soft UOA         = compiler.llvm (1c2eb494b8ae3bc4) 
+# Host OS UOA      = linux-64 (4258b5fe54828a50)
+# Target OS UOA    = linux-64 (4258b5fe54828a50)
+# Target OS bits   = 64
+# Tool version     = local
+# Tool int version = 88888
+
+export CK_AR=ar
+export CK_ASM_EXT=.s
+export CK_CC=clang
+export CK_COMPILER_FLAGS_OBLIGATORY=
+export CK_CXX=clang++
+export CK_DLL_EXT=.so
+export CK_EXE_EXT=.out
+export CK_EXTRA_LIB_DL=-ldl
+export CK_EXTRA_LIB_M=-lm
+export CK_FLAGS_CREATE_ASM=-S
+export CK_FLAGS_CREATE_OBJ=-c
+export CK_FLAGS_DLL="-shared -fPIC"
+export CK_FLAGS_DLL_EXTRA=
+export CK_FLAGS_OUTPUT="-o "
+export CK_FLAGS_STATIC_BIN="-static -fPIC"
+export CK_FLAGS_STATIC_LIB=-fPIC
+export CK_FLAG_PREFIX_INCLUDE=-I
+export CK_FLAG_PREFIX_VAR=-D
+export CK_LB="ar rcs"
+export CK_LB_OUTPUT="-o "
+export CK_LD_FLAGS_EXTRA=
+export CK_LIB_EXT=.a
+export CK_MAKE=make
+export CK_OBJDUMP="objdump -d"
+export CK_OBJ_EXT=.o
+export CK_OPT_SIZE=-Os
+export CK_OPT_SPEED=-O3
+export CK_OPT_SPEED_SAFE=-O2
+export CK_PLUGIN_FLAG=-fplugin=
+export CK_PROFILER=gprof
+
