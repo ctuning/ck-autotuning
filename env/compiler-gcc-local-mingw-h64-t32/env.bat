96d58a1bb4cb4d3df1042a5c3129c491254e6079
Grigori Fursin <Grigori.Fursin@cTuning.org>
2015-04-02 19:29:09 +0100

    adding host and target OS for local GCC/LLVM

---
+++ env/compiler-gcc-local-mingw-h64-t32/env.bat
@@ -0,0 +1,48 @@
+@echo off
+
+rem CK generated script
+
+rem Soft UOA         = compiler.gcc (3a8a82fa40bf992d) 
+rem Host OS UOA      = windows-64 (7a95e0754c37610a)
+rem Target OS UOA    = mingw-32 (d4c3a9e4a9a77677)
+rem Target OS bits   = 32
+rem Tool version     = local
+rem Tool int version = 88888
+
+set CK_AR=ar
+set CK_ASM_EXT=.s
+set CK_CC=gcc
+set CK_COMPILER_FLAGS_OBLIGATORY= -DWINDOWS
+set CK_CXX=g++ -fpermissive
+set CK_DLL_EXT=.so
+set CK_EXE_EXT=.out
+set CK_EXTRA_LIB_DL=-ldl
+set CK_EXTRA_LIB_M=-lm
+set CK_F90=gfortran
+set CK_F95=gfortran
+set CK_FC=gfortran
+set CK_FLAGS_CREATE_ASM=-S
+set CK_FLAGS_CREATE_OBJ=-c
+set CK_FLAGS_DLL=-shared -fPIC
+set CK_FLAGS_DLL_EXTRA=
+set CK_FLAGS_OUTPUT=-o 
+set CK_FLAGS_STATIC_BIN=-static -fPIC
+set CK_FLAGS_STATIC_LIB=-fPIC
+set CK_FLAG_PREFIX_INCLUDE=-I
+set CK_FLAG_PREFIX_VAR=-D
+set CK_LB=ar rcs
+set CK_LB_OUTPUT=-o 
+set CK_LD=ld
+set CK_LD_FLAGS_EXTRA=
+set CK_LIB_EXT=.a
+set CK_MAKE=mingw32-make
+set CK_OBJDUMP=objdump -d
+set CK_OBJ_EXT=.o
+set CK_OPT_SIZE=-Os
+set CK_OPT_SPEED=-O3
+set CK_OPT_SPEED_SAFE=-O2
+set CK_PLUGIN_FLAG=-fplugin=
+set CK_PROFILER=gprof
+
+
+exit /b 0
