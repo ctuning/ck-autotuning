3d166def5d6596a867f73054cd6cb84082d8c882
Grigori Fursin <grigori.fursin@cTuning.org>
2015-09-09 23:46:26 +0200

    * adding function 'import_all_files' in 'dataset' to automatically register all dataset files

--- module/dataset/module.py
+++ module/dataset/module.py
@@ -29,3 +29,57 @@ def init(i):
 
     """
     return {'return':0}
+
+##############################################################################
+# Import all files to meta
+
+def import_all_files(i):
+    """
+    Input:  {
+               data_uoa
+               (repo_uoa)
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    import os
+
+    duoa=i['data_uoa']
+    ruoa=i.get('repo_uoa','')
+
+    r=ck.access({'action':'load',
+                 'module_uoa':work['self_module_uid'],
+                 'data_uoa':duoa,
+                 'repo_uoa':ruoa})
+    if r['return']>0: return r
+
+    duid=r['data_uid']
+    d=r['dict']
+    p=r['path']
+
+    if 'dataset_files' not in d: d['dataset_files']=[]
+    dfiles=d['dataset_files']
+
+    dirList=os.listdir(p)
+    for fn in dirList:
+         p1=os.path.join(p, fn)
+         if os.path.isfile(p1):
+            if fn not in dfiles:
+               dfiles.append(fn)
+
+    r=ck.access({'action':'update',
+                 'module_uoa':work['self_module_uid'],
+                 'data_uoa':duid,
+                 'repo_uoa':ruoa,
+                 'dict':d,
+                 'substitute':'yes',
+                 'sort_keys':'yes'})
+    if r['return']>0: return r
+
+    return {'return':0}
