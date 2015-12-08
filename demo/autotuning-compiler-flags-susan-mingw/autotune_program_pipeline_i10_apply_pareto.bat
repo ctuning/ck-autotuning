0b5bc7fd1db40f24f2ff3f3285479bafb0b369aa
Grigori Fursin <grigori.fursin@cTuning.org>
2015-12-08 14:43:13 +0100

    fixing typo in autotuning demo

--- demo/autotuning-compiler-flags-susan-mingw/autotune_program_pipeline_i10_apply_pareto.bat
+++ demo/autotuning-compiler-flags-susan-mingw/autotune_program_pipeline_i10_apply_pareto.bat
@@ -1,3 +1,3 @@
-rem call ck rm experiment:demo-autotune-flags-susan-mingw-i10-pareto
-rem call ck cp experiment:demo-autotune-flags-susan-mingw-i10 :demo-autotune-flags-susan-mingw-i10-pareto
+call ck rm experiment:demo-autotune-flags-susan-mingw-i10-pareto
+call ck cp experiment:demo-autotune-flags-susan-mingw-i10 :demo-autotune-flags-susan-mingw-i10-pareto
 call ck autotune pipeline:program pipeline_from_file=_setup_program_pipeline_tmp.json @autotune_program_pipeline_i10_apply_pareto.json %*
