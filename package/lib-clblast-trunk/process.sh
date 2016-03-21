18b9a3d7bc351d91d46c87a96f13e90819167e86
Grigori Fursin <grigori.fursin@cTuning.org>
2016-03-21 14:55:41 +0100

    adding universal OpenCL stubs package (1.2) targeting Android devices

---
+++ package/lib-clblast-trunk/process.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#
+# Installation script for CK packages.
+#
+# See CK LICENSE.txt for licensing details.
+# See CK Copyright.txt for copyright details.
+#
+# Developer(s): Grigori Fursin, 2015
+#
+
+# PACKAGE_DIR
+# INSTALL_DIR
+
+export LIB_NAME=libopenme
+
+echo ""
+echo "Obtaining latest OpenME from GitHub ..."
+echo ""
+
+cd ${INSTALL_DIR}
+
+git clone https://github.com/ctuning/openme.git openme
+
+cd openme
+git pull
+
+cd src/c
+
+. ./ck-make.sh
+ if [ "${?}" != "0" ] ; then
+  echo "Error: Compilation failed in $PWD!" 
+  exit 1
+ fi
