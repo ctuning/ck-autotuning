64c94cc9d06c84ff889b708485f9be881ea88a1c
Grigori Fursin <grigori.fursin@cTuning.org>
2017-05-18 16:03:19 +0200

    added example of simplified and customized compiler autotuning (script:customized-autotuning)

---
+++ demo/customized-autotuning/autotune_customized_universal.bat
@@ -0,0 +1,1 @@
+ck autotune program:*susan @autotune_customized_universal.json --new --skip_collaborative --scenario=experiment.tune.compiler.flags --extra_tags=explore
---
+++ demo/customized-autotuning/autotune_customized_universal1.bat
@@ -0,0 +1,1 @@
+ck autotune program:*susan @autotune_customized_universal1.json --new --skip_collaborative --scenario=experiment.tune.compiler.flags --extra_tags=explore
