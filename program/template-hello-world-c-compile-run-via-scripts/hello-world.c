3c9f48890aa07bba8b1c89d2ace56aecacf00bd5
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-05-25 15:44:04 +0200

    added program template with compile and run scripts

---
+++ program/template-hello-world-c-compile-run-via-scripts/hello-world.c
@@ -0,0 +1,31 @@
+/*
+ CK program template
+
+ See CK LICENSE.txt for licensing details
+ See CK COPYRIGHT.txt for copyright details
+
+ Developer: Grigori Fursin, 2018, Grigori.Fursin@cTuning.org, http://fursin.net
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+  char* env;
+
+  printf("Hello world!\n\n");
+
+  env=getenv("CK_VAR1");
+  if (env!=NULL) {
+    printf("CK_VAR1=%s\n",env);
+  }
+
+  env=getenv("CK_VAR2");
+  if (env!=NULL) {
+    printf("CK_VAR2=%s\n",env);
+  }
+
+  return 0;
+}
